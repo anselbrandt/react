ece95e1b980487869f90fd6c6ad14b36a267c06c
anselbrandt <ansel@anselbrandt.com>
2021-01-11 00:56:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Typescript Template
+
+```bash
+yarn
+yarn start
+```
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">react-ts</div>;
+}
+
+export default App;
